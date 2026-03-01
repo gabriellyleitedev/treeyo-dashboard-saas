f2bc34563fd55118378b6ad18494da48d5e160b0
Gabrielly dos Santos Leite <gabriellydossantosleite3@gmail.com>
2026-03-01 01:08:56 -0300

    alguma alteração

--- src/components/MainChart.jsx
+++ src/components/MainChart.jsx
@@ -63,7 +63,7 @@ export default function MainChart() {
             data={data}
             margin={{
               top: 10,
-              right: isMobile ? 15 : 0,
+              right: isMobile ? 10 : 0,
               left: isMobile ? -45 : -15,
               bottom: 0
             }}
