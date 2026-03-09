c4721d611ff4824ac093d807610bb0d8611b51fd
Gabrielly dos Santos Leite <gabriellydossantosleite3@gmail.com>
2026-03-09 00:00:23 -0300

    alguma alteração

--- src/pages/Lancamento.jsx
+++ src/pages/Lancamento.jsx
@@ -90,11 +90,11 @@ const Lançamento = () => {
             <div className='pointer-events-none absolute top-0 left-1/2 -translate-x-1/2 w-1/2 h-22 bg-gradient-to-r from-transparent via-[#1fba11]/40 to-transparent blur-[60px] -rotate-12 '></div>
 
             {/* --- HEADER MOBILE (Exclusivo Mobile) --- */}
-            <div className="md:hidden flex items-center justify-between px-6 py-0 relative pt-2 ">
+            <div className="md:hidden flex items-center justify-between px-4 py-0 relative pt-1 ">
                 <div className="absolute inset-0 -z-10  rounded-md" />
 
                 {/* Perfil Mobile */}
-                <div className="md:hidden flex items-center justify-between px-0 py-2 sticky top-0 z-50 ">
+                <div className="md:hidden flex items-center justify-between px-0 py-3 sticky top-0 z-50 ">
                     <div className="flex items-center gap-2">
                         {/* Botão Voltar - Substitui a foto */}
                         <button
@@ -129,11 +129,11 @@ const Lançamento = () => {
   </div>
 </div>
 */}
-            <motion.div className="w-full h-full flex flex-col" initial="hidden" animate="visible" variants={container}>
+            <motion.div className="w-full h-full flex flex-col " initial="hidden" animate="visible" variants={container}>
 
 
                 {/* HEADER DESKTOP (md:flex) */}
-                <motion.header className="hidden md:flex flex-row items-center justify-between w-full h-14 gap-2 shrink-0 px-4 mt-4" variants={item}>
+                <motion.header className="hidden md:flex flex-row items-center justify-between w-full h-18 gap-2 shrink-0 px-4 mt-4 " variants={item}>
                     <div>
                         <h1 className="text-gray-200 font-semibold text-2xl whitespace-nowrap">
                             <span className="text-neutral-400 font-normal"> Dashboard / </span> Lançamento
@@ -148,35 +148,35 @@ const Lançamento = () => {
                             className="bg-black/20 text-sm text-gray-200 pl-10 pr-4 py-2 rounded-full border border-white/10 w-48 h-8 focus:w-54 focus:outline-none focus:border-green-500/20 transition-all duration-300 placeholder:text-neutral-600 cursor-pointer"
                         />
                     </div>
-                    <div className="ml-auto flex items-center gap-3"> 
-    
-    {/* Container do Sol/Lua */}
-    <div className="flex items-center p-0.5 rounded-full bg-black/40 border border-white/10">
-                               <button
-                                 onClick={() => !isDarkMode && toggleTheme()}
-                                 className={`w-9 h-9 flex items-center justify-center rounded-full transition cursor-pointer ${!isDarkMode ? 'bg-[#333333] text-white shadow-md' : 'text-gray-500 hover:bg-white/5'}`}
-                               >
-                                 <Sun size={16} />
-                               </button>
-                               <button
-                                 onClick={() => isDarkMode && toggleTheme()}
-                                 className={`w-9 h-9 flex items-center justify-center rounded-full transition cursor-pointer ${isDarkMode ? 'bg-[#333333] text-white shadow-md' : 'text-gray-500 hover:bg-white/5'}`}
-                               >
-                                 <Moon size={16} />
-                               </button>
-                             </div>
-                             <div className="w-10 h-10 rounded-full border bg-white/5 border-white/5 hover:bg-white/5 transition flex items-center justify-center cursor-pointer relative">
-                                         <Bell size={20} className="text-gray-200" />
-                                         <span className="absolute top-2 right-2.5 w-2 h-2 bg-[#1fba11] rounded-full border border-white/10" />
-                                       </div>
-                                    </div>
+                    <div className="ml-auto flex items-center gap-3 ">
+
+                        {/* Container do Sol/Lua */}
+                        <div className="flex items-center p-0.5 rounded-full bg-black/40 border border-white/10 ">
+                            <button
+                                onClick={() => !isDarkMode && toggleTheme()}
+                                className={`w-9 h-9 flex items-center justify-center rounded-full transition cursor-pointer ${!isDarkMode ? 'bg-[#333333] text-white shadow-md' : 'text-gray-500 hover:bg-white/5'}`}
+                            >
+                                <Sun size={16} />
+                            </button>
+                            <button
+                                onClick={() => isDarkMode && toggleTheme()}
+                                className={`w-9 h-9 flex items-center justify-center rounded-full transition cursor-pointer ${isDarkMode ? 'bg-[#333333] text-white shadow-md' : 'text-gray-500 hover:bg-white/5'}`}
+                            >
+                                <Moon size={16} />
+                            </button>
+                        </div>
+                        <div className="w-10 h-10 rounded-full border bg-white/5 border-white/5 hover:bg-white/5 transition flex items-center justify-center cursor-pointer relative">
+                            <Bell size={20} className="text-gray-200" />
+                            <span className="absolute top-2 right-2.5 w-2 h-2 bg-[#1fba11] rounded-full border border-white/10" />
+                        </div>
+                    </div>
                 </motion.header>
 
                 <motion.div variants={item} className="w-full bg-gradient-to-r from-transparent via-white/20 to-transparent mb-8 mt-6 md:mt-10 h-px shrink-0" />
 
                 {/* Filtros de Tipo */}
                 <div className="shrink-0 px-4 md:px-0">
-                    <motion.div variants={item} className="grid grid-cols-12 gap-6 w-full">
+                    <motion.div variants={item} className="sm:grid grid-cols-12 gap-6 w-full">
                         <div className="flex flex-wrap gap-3 w-fit ml-3">
                             {["Entrada", "Saída", "Investimento"].map((tipo) => {
                                 const cores = getCoresDinamicas(tipo);
@@ -195,6 +195,42 @@ const Lançamento = () => {
                         </div>
                     </motion.div>
                 </div>
+
+ <div className="shrink-0 ">
+                   
+
+                   
+              
+                    <motion.div variants={itemVariants}>
+                         {/*
+                        <div className="col-span-12 lg:col-span-4 pr-10 ">
+                            <FormularioLancamento
+                                tipoSelecionado={TipoAtivo}
+                                aoConfirmar={(novo) => setLista([novo, ...lista])}
+                            />
+                        </div>
+                         */}
+                        </motion.div>
+                </div>
+                
+
+                
+                <motion.div variants={itemVariants} className="shrink-0">
+                    {/*
+                    <div className="col-span-12 flex justify-center mt-10">
+                        {listaGeral.length > 0 ? (
+                            <CardLancamento lancamento={listaGeral[0]} />
+                        ) : (
+                            <div className="w-full -translate-y-32 -translate-x-20 max-w-[420px] h-[150px] border border-dashed border-white/10 rounded-2xl flex items-center justify-center text-neutral-600 text-sm italic">
+                                Nenhum lançamento de {TipoAtivo} para exibir no card.
+                            </div>
+                        )}
+                    </div>
+                    */}
+                </motion.div>
+
+
+
 
                 {/* LISTA DE ÚLTIMOS LANÇAMENTOS */}
                 <motion.div variants={itemVariants} className="w-full flex-1 min-h-0 flex flex-col mt-10 md:mt-16 px-4 md:px-10 overflow-hidden">
